088368f834f0fb93ffc457a0e02b752f4a564684
matheus weber <matheus.jk.weber@gmail.com>
2023-04-25 13:19:40 -0300

    Details VC

---
+++ GarupaTest/Features/BeerDetail/BeerDetailViewController/BeerDetailViewController.swift
@@ -0,0 +1,8 @@
+//
+//  BeerDetailViewController.swift
+//  GarupaTest
+//
+//  Created by Matheus Weber on 24/04/23.
+//
+
+import Foundation
---
+++ GarupaTest/Features/BeerDetail/View/BeerFoodPairingTableViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  BeerFoodPairingTableViewCell.swift
+//  GarupaTest
+//
+//  Created by Matheus Weber on 24/04/23.
+//
+
+import Foundation
